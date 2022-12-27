ca65e37ded21642dc81998afb9a6772251c8e541
Steven Cobb <sjcobb.dev@gmail.com>
2022-12-27 14:54:24 -0500

    cleanup next.config.js, add lodash-es to transpilePackages

--- next.config.js
+++ next.config.js
@@ -9,20 +9,15 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  reactStrictMode: false,
-  // reactStrictMode: true,
+  reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['replicate.com', 'replicate.delivery'],
   },
   experimental: {
-    // transpilePackages: [
-    //   '@perses-dev/dashboards',
-    //   'echarts',
-    //   'zrender',
-    // ],
     appDir: false,
   },
+  // https://nextjs.org/blog/next-13-1#built-in-module-transpilation-stable
   transpilePackages: [
     '@perses-dev/components',
     '@perses-dev/core',
@@ -32,39 +27,8 @@ const nextConfig = {
     '@perses-dev/prometheus-plugin',
     'echarts',
     'zrender',
+    'lodash-es',
   ],
-  // // transpilePackages: ['lodash-es'], // https://nextjs.org/blog/next-13-1#built-in-module-transpilation-stable
-  // // // transpileModules: ['lodash-es'],
 };
 
 module.exports = nextConfig;
-
-// // // https://stackoverflow.com/a/65939797/17575201
-// // const withTM = require("next-transpile-modules")(["mojave/classes"]);
-
-// // const withTM = require('next-transpile-modules')(['lodash-es']); // pass the modules you would like to see transpiled
-// const withTM = require('next-transpile-modules')(['echarts', 'zrender']); // pass the modules you would like to see transpiled
-// module.exports = withTM(nextConfig);
-
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   images: {
-//     domains: ['replicate.com', 'replicate.delivery'],
-//   },
-//   transpilePackages: ['echarts', 'zrender'],
-//   // transpilePackages: ['lodash-es'], // https://nextjs.org/blog/next-13-1#built-in-module-transpilation-stable
-//   // // transpileModules: ['lodash-es'],
-//   experimental: {
-//     appDir: false,
-//   },
-// };
-
-// module.exports = nextConfig;
-
-// // // https://nextjs.org/docs/api-reference/next.config.js/introduction
-// // module.exports = ({ nextConfig }) => {
-// //   return {
-// //     ...nextConfig
-// //   }
-// // }

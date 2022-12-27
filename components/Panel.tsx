5f4af3ebef728cc094a340d61d59bb4a3a8ad048
Steven Cobb <sjcobb.dev@gmail.com>
2022-12-26 22:53:21 -0500

    separate out utils, add panel-gen ex, render LineChart workaround

--- components/Panel.tsx
+++ components/Panel.tsx
@@ -1,5 +1,6 @@
 import { ChartsThemeProvider, LineChart, PersesChartsTheme } from '@perses-dev/components';
 import { Box } from '@mui/material';
+import { formatMetrics } from '../utils';
 
 // https://stackoverflow.com/questions/60738486/nextjs-react-webpackerror-window-is-not-defined/61840658#61840658
 // import dynamic from 'next/dynamic';
@@ -43,96 +44,6 @@ const mockData = [
     ],
   },
 ];
-
-function formatMetrics(queryResult) {
-  const graphData = { timeSeries: [], xAxis: [] };
-  const xValues = new Set();
-  for (const result of queryResult) {
-    const yValues = [];
-    if ('values' in result) {
-      for (const [x, y] of result.values) {
-        xValues.add(Number(x) * 1000);
-        yValues.push(Number(y));
-      }
-      const name = getUniqueKeyForPrometheusResult(result.metric, true);
-      const lineSeries = getLineSeries(name, yValues);
-      graphData.timeSeries.push(lineSeries);
-    }
-  }
-  graphData.xAxis = Array.from(xValues).sort();
-  return graphData;
-}
-
-// https://echarts.apache.org/en/option.html#series-line.type
-function getLineSeries(name, data) {
-  return {
-    type: 'line',
-    name: name,
-    data: data,
-    color: getRandomColor(name),
-    showSymbol: false,
-    symbol: 'circle',
-    sampling: 'lttb',
-    lineStyle: {
-      width: 1,
-    },
-    emphasis: {
-      lineStyle: {
-        width: 1.5,
-      },
-    },
-    markLine: {},
-  };
-}
-
-function stringifyPrometheusMetricLabels(labels, removeExprWrap) {
-  const labelStrings = [];
-  Object.keys(labels)
-    .sort()
-    .forEach((labelName) => {
-      const labelValue = labels[labelName];
-      if (typeof labelValue === 'string') {
-        if (removeExprWrap) {
-          labelStrings.push(`"${labelName}":"${labelValue}"`);
-        } else {
-          labelStrings.push(`${labelName}="${labelValue}"`);
-        }
-      }
-    });
-  return `{${labelStrings.join(',')}}`;
-}
-
-function getUniqueKeyForPrometheusResult(metricLabels, removeExprWrap) {
-  const metricNameKey = '__name__';
-  if (!!metricLabels) {
-    if (metricLabels.hasOwnProperty(metricNameKey)) {
-      const stringifiedLabels = stringifyPrometheusMetricLabels(
-        {
-          ...metricLabels,
-          [metricNameKey]: undefined,
-        },
-        removeExprWrap
-      );
-      if (removeExprWrap) {
-        return `${stringifiedLabels}`;
-      } else {
-        return `${metricLabels[metricNameKey]}${stringifiedLabels}`;
-      }
-    }
-    return stringifyPrometheusMetricLabels(metricLabels, removeExprWrap);
-  }
-  return '';
-}
-
-function getRandomColor(identifier) {
-  let hash = 0;
-  for (let index = 0; index < identifier.length; index++) {
-    hash = identifier.charCodeAt(index) + ((hash << 5) - hash);
-  }
-  // Use HSLA to only get random "bright" colors from this
-  const color = `hsla(${~~(180 * hash)},50%,50%,0.8)`;
-  return color;
-}
 
 const testChartsTheme: PersesChartsTheme = {
   echartsTheme: {},
